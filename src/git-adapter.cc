56f982a54aa23246c543ce138f0314d0b1228599
sw4k <161171772+sw4k@users.noreply.github.com>
2024-04-25 20:24:40 -0400

    fix: order `waitpid` after pipe consumption

--- src/git-adapter.cc
+++ src/git-adapter.cc
@@ -158,13 +158,13 @@ namespace csemver
             close(stdout_pipe[1]);
             close(stderr_pipe[1]);
 
+            ConsumeGitOutput(stdout_pipe[0], stderr_pipe[0]);
             int wait_result = waitpid(gitPid, NULL, 0);
             if (-1 == wait_result)
             {
                 auto eno = errno;
                 _logger->Error("waitpid error: " + eno);
             }
-            ConsumeGitOutput(stdout_pipe[0], stderr_pipe[0]);
 
             // close remaining pipe fds
             close(stdin_pipe[1]);
